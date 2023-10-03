4b021d7c14377f646e5946dacf7459bdbfd43cf8
factism001 <factism001@gmail.com>
2023-10-03 15:46:07 +0100

    First Bug Application Commit

--- Djangoproject/bug/models.py
+++ Djangoproject/bug/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Bug(models.Model):
+    BUG_TYPES = (
+        ('error', 'Error'),
+        ('new_feature', 'New Feature'),
+        ('enhancement', 'Enhancement'),
+        ('other', 'Other'),
+    )
+
+    STATUS_CHOICES = (
+        ('todo', 'To Do'),
+        ('inprogress', 'In Progress'),
+        ('done', 'Done'),
+    )
+
+    description = models.TextField()
+    bug_type = models.CharField(max_length=20, choices=BUG_TYPES)
+    report_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.description
+
